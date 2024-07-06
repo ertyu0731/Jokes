2ee8d76ac30252e22b182e3d4dc3b5e88e2291b6
kmy <kmy_er_sze_lei@s2023.ssts.edu.sg>
2024-07-06 16:06:23 +0800

    Initial commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Kmy Er on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
